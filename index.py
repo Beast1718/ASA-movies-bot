0ed0b515037fce509389c8ee3ec3284c0ddb0ca9
Beast1718 <125872726+Beast1718@users.noreply.github.com>
2023-02-20 16:18:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,9 +8,9 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 
-TOKEN = os.getenv("5858004780:AAFA2q1zFSExnlBXtSHHnngM4RRcCgQ0P1U")
-URL = "https://asa-movies-bot.vercel.app"
-bot = Bot("5858004780:AAFA2q1zFSExnlBXtSHHnngM4RRcCgQ0P1U")
+TOKEN = "5858004780:AAFA2q1zFSExnlBXtSHHnngM4RRcCgQ0P1U"
+URL = "https://asa-movies-bot-git-main-beast1718.vercel.app"
+bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
